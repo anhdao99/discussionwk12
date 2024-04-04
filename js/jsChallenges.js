fa277bbff409af61ebdc9a6941834801a0d29733
Viet Anh Dao <143753140+anhdao99@users.noreply.github.com>
2024-04-05 03:12:02 +0700

    Add files via upload

---
+++ js/jsChallenges.js
@@ -0,0 +1,34 @@
+document.querySelector("#subscribe").addEventListener("click", function(){
+    if(this.checked){
+        document.getElementById("emailDiv").style.display = "block";
+    } else {
+        document.getElementById("emailDiv").style.display = "none";
+    }
+});
+
+document.addEventListener("click", function(){
+    alert(new Date());
+});
+
+let sameAddress = document.getElementById('sameAddress');
+let home = document.getElementById('home');
+let bill = document.getElementById('bill');
+sameAddress.addEventListener('change', function() {
+    if (sameAddress.checked) {
+        home.value = bill.value;
+        home.disabled = true;
+    } else {
+        home.value = '';
+    }
+});
+
+function challenge3() {
+    let standing = document.querySelector('input[name="standing"]:checked');
+    let gradDate = document.querySelector('input[name="gradDate"]:checked');
+    if (standing && gradDate) {
+      return true;
+    } else {
+      alert("Please select both options");
+      return false;
+    }
+  }
